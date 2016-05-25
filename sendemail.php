a3e5de3eaf8590bd365d617ea4ffad68f1df4c99
qiip <online878@gmail.com>
2016-05-26 00:30:01 +0300

    laser

--- sendemail.php
+++ sendemail.php
@@ -3,20 +3,17 @@
 header('Content-type: application/json'); 
 $status = array( 
 'type'=>'success', 
-'message'=>'Спасибо за заявку! Наши менеджеры свяжутся с вами в ближайшее время.' 
+'message'=>'Спасибо за заявку! Наши менеджеры свяжутся с вами в ближайшее время' 
 );
   
     $name = @trim(stripslashes($_POST['name']));
-    $whours = @trim(stripslashes($_POST['whours']));  
-    $select = @trim(stripslashes($_POST['select']));  
     $phone = @trim(stripslashes($_POST['phone']));  
-    $message = @trim(stripslashes($_POST['message']));  
  
     $email_from = $email; 
-    $email_to = 'frolov@ukr.net';//replace with your email
+    $email_to = 'chebanova_sveta@mail.ru, order@lazer-center.od.ua';//replace with your email
  
  
-    $body = 'Имя: ' . $name . "\n\n" . 'Количество рабочих чаосв: ' . $whours . "\n\n" . 'Вид техники: ' . $select . "\n\n" . 'Телефон: ' . $phone . "\n\n" . 'Сообщение: ' . $message; 
+    $body = 'Имя: ' . $name . "\n\n" . 'Телефон: ' . $phone; 
  
     $success = @mail($email_to, $phone, $body, 'From: <'.$email_from.'>'); 
  
